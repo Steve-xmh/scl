e643e552e10c932cb8e305615c84f1e6e7fa1c75
Steve-xmh <Steve-xmh@users.noreply.github.com>
2022-11-20 02:41:44 +0000

    deploy: 924d0038b1c2fa447dcbd5acbe39647b258c9fed

--- scl-doc/implementors/core/convert/trait.AsRef.js
+++ scl-doc/implementors/core/convert/trait.AsRef.js
@@ -1,3 +1,3 @@
-(function() {var implementors = {};
-implementors["scl_core"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.64.0/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.64.0/std/primitive.str.html\">str</a>&gt; for <a class=\"enum\" href=\"scl_core/utils/enum.Arch.html\" title=\"enum scl_core::utils::Arch\">Arch</a>","synthetic":false,"types":["scl_core::utils::Arch"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {
+"scl_core":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.65.0/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.65.0/std/primitive.str.html\">str</a>&gt; for <a class=\"enum\" href=\"scl_core/utils/enum.Arch.html\" title=\"enum scl_core::utils::Arch\">Arch</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
